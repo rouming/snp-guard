02b419d617fb66a7bc224b4e93839839d9642992
Roman Penyaev <r.peniaev@gmail.com>
2026-01-26 14:51:38 +0100

    initramfs-tools/hook.sh: no need to explicitly add modules
    force_load() function is able to resolve all dependencies

--- scripts/initramfs-tools/hook.sh
+++ scripts/initramfs-tools/hook.sh
@@ -11,18 +11,6 @@ esac
 # Required by initramfs-tools
 . /usr/share/initramfs-tools/hook-functions
 
-# Deploy crypt modules, dependencies of the SEV-SNP driver
-manual_add_modules \
-    crypto \
-    cryptd \
-    aes \
-    aesni_intel \
-    cbc \
-    xts \
-    sha256 \
-    sha512 \
-    dm_crypt
-
 # SEV-SNP guest driver
 force_load sev-guest
 
